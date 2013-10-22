a73a1c853d67331fff97b41cda6bd6ecc0c44b24
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-10-22 16:13:51 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Abtakeoverab::Application.routes.draw do
   scope "(:locale)", :locale => /en|fi|sv/ do
     resources :announcements
     devise_for :users, :controllers => {:registrations => "registrations"}
-    root :to => "announcements#index"
   end
 
   root :to => "announcements#index"
